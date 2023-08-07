8e6955430b61934731c01b2b8cfefa1ddbabfd83
Shannon Millar <shanenak@gmail.com>
2023-08-07 10:57:54 -0700

    Start Knight class

---
+++ polytreenode.rb
@@ -0,0 +1,76 @@
+class PolyTreeNode
+    attr_reader :parent, :children, :value
+
+    def initialize(position)
+        @parent = parent
+        @position = position # instance
+        @children = [] # array of instances
+        # @value = value # string
+        # @root_node = root_node
+    end
+
+    def parent=(new_parent) # nil or an instance
+        @parent.children.delete(self) if @parent != nil && @parent.children.include?(self)  
+        @parent = new_parent
+        if new_parent == nil   
+            return nil
+        elsif !new_parent.children.include?(self)
+            new_parent.children << self
+        end
+    end
+
+    def add_child(child)
+        child.parent = self
+        # @children << child
+    end
+
+    def remove_child(child)
+        raise "Node is not a child" unless self.children.include?(child)
+        child.parent = nil
+    end
+
+    def inspect
+        return "<# Value: #{@value} Parent: #{@parent} Children: #{@children} "
+    end
+
+    # def dfs(target)
+    #     tree = self.dup
+    #     stack = self
+    #     self = self.children
+    #     while stack.length > 0 
+    #         if stack[-1] == target
+    #             return self
+    #         elsif stack[-1] != target
+    #             stack.pop
+    #             stack << stack[-1].children
+    #         end
+    #     end
+    # end
+
+    def dfs(target)
+        return self if self.value == target
+        return nil if self.children.empty?
+        # nodes = self
+        self.children.each do |node| 
+            result = node.dfs(target)
+            return result unless result.nil?
+        end
+        nil
+    end
+
+    def bfs(target)
+        queue = [self]
+        while queue.length > 0
+            if queue[0].value == target
+                return queue[0]
+            else
+                queue += queue[0].children # we only had to switch these two lines!! line 75 and 76
+                queue.shift
+            end
+            # end
+        end
+
+        return  nil
+    end
+    
+end
