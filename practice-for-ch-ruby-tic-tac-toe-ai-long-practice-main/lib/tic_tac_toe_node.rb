93e3d009171da431a49f40351bc82c4d9362ad71
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-07 15:09:01 -0700

    Start children instance method

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/tic_tac_toe_node.rb
@@ -1,7 +1,11 @@
 require_relative 'tic_tac_toe'
 
 class TicTacToeNode
+  attr_reader :board, :next_mover_mark, :prev_move_pos
   def initialize(board, next_mover_mark, prev_move_pos = nil)
+    @board = board
+    @next_mover_mark = next_mover_mark
+    @prev_move_pos = prev_move_pos unless prev_move_pos.nil?
   end
 
   def losing_node?(evaluator)
@@ -13,5 +17,7 @@ class TicTacToeNode
   # This method generates an array of all moves that can be made after
   # the current move.
   def children
+    @board.open_positions.each do |position|
+      next_node = TicTacToeNode.new(@board, position, self)
   end
 end
