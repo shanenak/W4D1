1c199429b06990c6fdeb5b923c297a13a24fd304
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-07 14:12:24 -0700

    Finish build_move_tree instance method

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -7,7 +7,9 @@ class KnightPathFinder
         (0...nums.length).each do |first|
             (0...nums.length).each do |second|
                 if nums[first].abs + nums[second].abs == 3
-                    pairs << [pos[0]-nums[first], pos[1]-nums[second]]
+                    x = pos[0]-nums[first]
+                    y = pos[1]-nums[second]
+                    pairs << [x,y] if (x < 8 && y < 8) && (x > 0 && y > 0)
                 end
             end
         end
@@ -21,18 +23,32 @@ class KnightPathFinder
     end
 
     def build_move_tree
-        
+        queue = [@root_node] #
+        until queue.empty?
+            current_node = queue.first
+            valid_moves = new_move_positions(current_node.value)
+            valid_moves.each do |move|
+                child = PolyTreeNode.new(move)
+                current_node.add_child(child)
+                queue << child
+                #add valid moves to the queue and shift first element from queue
+            end
+            queue.shift
+        end
+        @root_node
     end
 
     def new_move_positions(pos)
         valid_moves = KnightPathFinder.valid_moves(pos)
-        valid_moves.select do |move|
+        filtered_moves = valid_moves.select do |move|
             !@considered_positions.include?(move)
         end
+        @considered_positions += filtered_moves
+        filtered_moves
     end
 
 
-    # n = KnightPathFinder.new([5,5])
-    # p n.new_move_positions([5,5])
-    # p KnightPathFinder.valid_moves([5,5])
+    n = KnightPathFinder.new([5,5])
+    p KnightPathFinder.valid_moves([5,5])
+    p n.build_move_tree
 end
