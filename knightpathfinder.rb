5299e7de2114d210004e9985624192e14529a30c
davislucky <58579323+davislucky@users.noreply.github.com>
2023-08-07 11:51:47 -0700

    Start valid_moves class method

--- knightpathfinder.rb
+++ knightpathfinder.rb
@@ -1,12 +1,33 @@
 require_relative 'polytreenode'
 class KnightPathFinder
+
+    def self.valid_moves
+        pairs = []
+        nums = [1,-1,-2,2]
+        (0...nums.length).each do |first|
+            (0...nums.length).each do |second|
+                if nums[first].abs + nums[second].abs == 3
+                    pairs << [nums[first], nums[second]]
+                end
+            end
+        end
+        pairs
+    end
+
+    p KnightPathFinder.valid_moves
+
+
     def initialize(pos)
         @pos = pos
         @root_node = PolyTreeNode.new(pos)
         # @node = PolyTreeNode.new(pos)
     end
 
-    def build_move_tree
-        # @root_node.children << polytreenode.new()
+    # def build_move_tree
+    #     # @root_node.children << polytreenode.new()
+    # end
+
+    def new_move_positions
+
     end
 end
