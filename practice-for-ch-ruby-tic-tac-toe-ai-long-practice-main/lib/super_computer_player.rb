e636e89d73222c35b4cdcf3eefa18034e79e3217
Shannon Millar <shanenak@gmail.com>
2023-08-07 17:28:55 -0700

    Almost complete computerplayer

--- practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/super_computer_player.rb
+++ practice-for-ch-ruby-tic-tac-toe-ai-long-practice-main/lib/super_computer_player.rb
@@ -2,6 +2,18 @@ require_relative 'tic_tac_toe_node'
 
 class SuperComputerPlayer < ComputerPlayer
   def move(game, mark)
+    node = TicTacToeNode.new(game.board, mark)
+    node.children.each do |child|
+      if child.winning_node?(mark)
+        return child.prev_move_pos 
+      elsif !child.losing_node(mark)
+        nonlosing_nodes = node.children.select {|child| !child.losing_node?(mark)}
+        return nonlosing_nodes.sample.prev_move_pos
+      else 
+        raise 'No non-losing nodes' if nonlosing_nodes.empty?
+        # return nonlosing_nodes.first.prev_move_pos
+      end
+    end
   end
 end
 
